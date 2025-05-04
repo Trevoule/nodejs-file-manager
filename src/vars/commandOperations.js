7855887f74eb13a0271d06bee1466e66c94f516d
Vladislav Reginevich <65953199+Trevoule@users.noreply.github.com>
2025-05-04 19:05:53 +0200

    fix: updated commandOpearations.js

--- src/vars/commandOperations.js
+++ src/vars/commandOperations.js
@@ -8,8 +8,8 @@ export const COMMAND_OPERATION = {
 
 export const COMMAND = {
   [COMMAND_OPERATION.navigation]: {
-    cd: 'cd',
     up: 'up',
+    cd: 'cd',
     ls: 'ls',
   },
   [COMMAND_OPERATION.fs]: {
@@ -41,6 +41,41 @@ export const COMMAND = {
   }
 };
 
+export const FULL_COMMAND_LIST = {
+  [COMMAND_OPERATION.navigation]: {
+    up: 'up',
+    cd: 'cd path_to_directory',
+    ls: 'ls',
+  },
+  [COMMAND_OPERATION.fs]: {
+    cat: 'cat path_to_file',
+    add: 'add new_file_name',
+    mkdir: 'mkdir new_directory_name',
+    rn: 'rn path_to_file new_filename',
+    cp: 'cp path_to_file path_to_new_directory',
+    mv: 'mv path_to_file path_to_new_directory',
+    rm: 'rm path_to_file',
+  },
+  [COMMAND_OPERATION.os]: {
+    eol: 'os --EOL',
+    cpus: 'os --cpus',
+    homedir: 'os --homedir',
+    username: 'os --username',
+    architecture: 'os --architecture',
+  },
+  [COMMAND_OPERATION.hash]: {
+    hash: 'hash path_to_file',
+  },
+  [COMMAND_OPERATION.zlib]: {
+    compress: 'compress path_to_file path_to_destination',
+    decompress: 'decompress path_to_file path_to_destination',
+  },
+
+  general: {
+    exit: '.exit',
+  }
+};
+
 const COMMAND_OPERATION_TITLE = {
   [COMMAND_OPERATION.navigation]: 'Navigation & working directory (nwd)',
   [COMMAND_OPERATION.fs]: 'Basic operations with files',
@@ -50,36 +85,36 @@ const COMMAND_OPERATION_TITLE = {
 };
 
 const navigationCommands = [
-  { command: COMMAND[COMMAND_OPERATION.navigation].up, description: 'Go upper from current directory' },
-  { command: COMMAND[COMMAND_OPERATION.navigation].cd, description: 'Go to dedicated folder from current directory' },
-  { command: COMMAND[COMMAND_OPERATION.navigation].ls, description: 'Print in console list of all files and folders in current directory' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.navigation].up, description: 'Go upper from current directory' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.navigation].cd, description: 'Go to dedicated folder from current directory' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.navigation].ls, description: 'Print in console list of all files and folders in current directory' },
 ];
 
 const fsCommands = [
-  { command: COMMAND[COMMAND_OPERATION.fs].cat, description: 'Read file and print its content in console' },
-  { command: COMMAND[COMMAND_OPERATION.fs].add, description: 'Create empty file in current working directory' },
-  { command: COMMAND[COMMAND_OPERATION.fs].mkdir, description: 'Create new directory in current working directory' },
-  { command: COMMAND[COMMAND_OPERATION.fs].rn, description: 'Rename file' },
-  { command: COMMAND[COMMAND_OPERATION.fs].cp, description: 'Copy file' },
-  { command: COMMAND[COMMAND_OPERATION.fs].mv, description: 'Move file' },
-  { command: COMMAND[COMMAND_OPERATION.fs].rm, description: 'Delete file' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].cat, description: 'Read file and print its content in console' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].add, description: 'Create empty file in current working directory' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].mkdir, description: 'Create new directory in current working directory' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].rn, description: 'Rename file' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].cp, description: 'Copy file' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].mv, description: 'Move file' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.fs].rm, description: 'Delete file' },
 ];
 
 const osCommands = [
-  { command: COMMAND[COMMAND_OPERATION.os].eol, description: 'Get EOL' },
-  { command: COMMAND[COMMAND_OPERATION.os].cpus, description: 'Get host machine CPUs info' },
-  { command: COMMAND[COMMAND_OPERATION.os].homedir, description: 'Get home directory and print it to console' },
-  { command: COMMAND[COMMAND_OPERATION.os].username, description: 'Get current system user name' },
-  { command: COMMAND[COMMAND_OPERATION.os].architecture, description: 'Get CPU architecture' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.os].eol, description: 'Get EOL' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.os].cpus, description: 'Get host machine CPUs info' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.os].homedir, description: 'Get home directory and print it to console' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.os].username, description: 'Get current system user name' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.os].architecture, description: 'Get CPU architecture' },
 ];
 
 const hashCommands = [
-  { command: COMMAND[COMMAND_OPERATION.hash].hash, description: 'Calculate hash for file and print it into console' }
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.hash].hash, description: 'Calculate hash for file and print it into console' }
 ];
 
 const zlibCommands = [
-  { command: COMMAND[COMMAND_OPERATION.zlib].compress, description: 'Compress file' },
-  { command: COMMAND[COMMAND_OPERATION.zlib].decompress, description: 'Decompress file' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.zlib].compress, description: 'Compress file' },
+  { command: FULL_COMMAND_LIST[COMMAND_OPERATION.zlib].decompress, description: 'Decompress file' },
 ];
 
 export const COMMAND_OPERATIONS = {
